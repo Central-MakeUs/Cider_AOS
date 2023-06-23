3ddab5e3faa505b22738ee2b4799edfbfda1fd8c
NaZe0320 <rwt1226@naver.com>
2023-06-24 00:37:52 +0900

    [init] DataBinding 설정

--- app/src/main/java/com/cider/cider/MainActivity.kt
+++ app/src/main/java/com/cider/cider/MainActivity.kt
@@ -2,10 +2,11 @@ package com.cider.cider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cider.cider.databinding.ActivityMainBinding
+import com.cider.cider.utils.binding.BindingActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/cider/cider/utils/binding/BindingActivity.kt
@@ -0,0 +1,17 @@
+package com.cider.cider.utils.binding
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BindingActivity<B: ViewDataBinding> (@LayoutRes private val layoutRes: Int): AppCompatActivity() {
+    lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        binding = DataBindingUtil.setContentView(this, layoutRes)
+    }
+}
