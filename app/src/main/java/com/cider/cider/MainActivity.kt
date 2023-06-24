7792a9dab5ae28ed55df04171860af8a33b1abb0
NaZe0320 <rwt1226@naver.com>
2023-06-24 11:53:26 +0900

    [init] hilt library 추가

--- app/src/main/java/com/cider/cider/MainActivity.kt
+++ app/src/main/java/com/cider/cider/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.cider.cider
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cider.cider.databinding.ActivityMainBinding
 import com.cider.cider.utils.binding.BindingActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
