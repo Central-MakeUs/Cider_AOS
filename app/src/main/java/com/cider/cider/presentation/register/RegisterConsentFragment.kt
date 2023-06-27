0ce57b7120bbece1a167bb2b6f3d278bebeda271
NaZe0320 <rwt1226@naver.com>
2023-06-27 22:41:57 +0900

    [feat] 자세히 보기 버튼 클릭 시 열리는 스크롤 구현

--- app/src/main/java/com/cider/cider/presentation/register/RegisterConsentFragment.kt
+++ app/src/main/java/com/cider/cider/presentation/register/RegisterConsentFragment.kt
@@ -2,6 +2,7 @@ package com.cider.cider.presentation.register
 
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup.LayoutParams
 import androidx.fragment.app.activityViewModels
 import com.cider.cider.R
 import com.cider.cider.databinding.FragmentRegisterConsentBinding
@@ -17,5 +18,42 @@ class RegisterConsentFragment
         super.onViewCreated(view, savedInstanceState)
         binding.register = viewModel
         viewModel.changeRegisterState(RegisterType.SERVICE_AGREEMENT)
+
+        setButton()
+    }
+
+    private fun setButton() {
+        binding.btnConsent2.setOnClickListener {
+            viewModel.setTermDetail(1)
+            if (viewModel.detailState.value == 1) {
+                binding.sv1.visibility = View.VISIBLE
+                binding.sv2.visibility = View.GONE
+
+                binding.btnConsent2.text = "접기"
+                binding.btnConsent3.text = "자세히 보기"
+
+                binding.layoutService.layoutParams.height = resources.getDimensionPixelSize(R.dimen.register_service_0dp)
+            } else {
+                binding.sv1.visibility = View.GONE
+                binding.btnConsent2.text = "자세히 보기"
+                binding.layoutService.layoutParams.height = LayoutParams.WRAP_CONTENT
+            }
+        }
+
+        binding.btnConsent3.setOnClickListener {
+            viewModel.setTermDetail(2)
+            if (viewModel.detailState.value == 2) {
+                binding.sv2.visibility = View.VISIBLE
+                binding.sv1.visibility = View.GONE
+
+                binding.btnConsent3.text = "접기"
+                binding.btnConsent2.text = "자세히 보기"
+                binding.layoutService.layoutParams.height = resources.getDimensionPixelSize(R.dimen.register_service_0dp)
+            } else {
+                binding.sv2.visibility = View.GONE
+                binding.btnConsent3.text = "자세히 보기"
+                binding.layoutService.layoutParams.height = LayoutParams.WRAP_CONTENT
+            }
+        }
     }
 }
--- app/src/main/java/com/cider/cider/utils/binding/BindingAdapter.kt
+++ app/src/main/java/com/cider/cider/utils/binding/BindingAdapter.kt
@@ -3,16 +3,17 @@ package com.cider.cider.utils.binding
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 
 
-@BindingAdapter("layout_height")
+@BindingAdapter("android:layout_height")
 fun setLayoutHeight(view: View, height: Int) {
-    val layoutParams: LayoutParams = view.layoutParams
-    layoutParams.height = if (height == 0) {
-        LayoutParams.WRAP_CONTENT
-    } else {
-        0
+    view.layoutParams = view.layoutParams.apply {
+        this.height = if (height == 0 ) {
+            LayoutParams.WRAP_CONTENT
+        } else {
+            0
+        }
     }
-    view.layoutParams = layoutParams
 }
