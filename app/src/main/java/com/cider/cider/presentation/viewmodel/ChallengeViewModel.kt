8c7fae10e3a47279664b9e721b7527cdb9c26930
NaZe0320 <rwt1226@naver.com>
2023-07-16 01:07:17 +0900

    [feat] api status 값에 따라 처리

--- app/src/main/java/com/cider/cider/presentation/viewmodel/ChallengeViewModel.kt
+++ app/src/main/java/com/cider/cider/presentation/viewmodel/ChallengeViewModel.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.cider.cider.domain.model.ChallengeCardModel
+import com.cider.cider.domain.model.ChallengeModel
+import com.cider.cider.domain.model.FeedModel
 import com.cider.cider.domain.type.challenge.Challenge
 import com.cider.cider.domain.type.challenge.ParticipationStatus
 import javax.inject.Inject
@@ -20,11 +22,12 @@ class ChallengeViewModel @Inject constructor(
     private val _categoryChallenge = MutableLiveData<List<ChallengeCardModel>>()
     val categoryChallenge: LiveData<List<ChallengeCardModel>> get() = _categoryChallenge
 
-    private val _feed = MutableLiveData<List<ChallengeCardModel>>()
-    val feed: LiveData<List<ChallengeCardModel>> get() = _feed
+    private val _feed = MutableLiveData<List<FeedModel>>()
+    val feed: LiveData<List<FeedModel>> get() = _feed
 
     init {
         testItem()
+        testFeed()
     }
 
     private fun testItem() {
@@ -81,6 +84,31 @@ class ChallengeViewModel @Inject constructor(
         _popularChallenge.value = list
         _officialChallenge.value = list
         _categoryChallenge.value = list
+
+    }
+
+    private fun testFeed() {
+        val list: MutableList<FeedModel> = mutableListOf()
+
+        list.add(
+            FeedModel(
+                id = 1, profile = null, nickname = "오늘챌린지화이팅", lv = 1,
+                date = "23.05.15 14:45", title = "오늘 챌린지 인증하는데", content = "하루 만보 걷기 챌린지는 쉽고 재미있게 만보를 걸을 수있는 챌린지로, 제가 맨날 일하다가 한번 입원하고 나서 심각성을 느끼고 만들게 된 멋진 챌린지",
+                imageList = null, challengeModel = ChallengeModel(id = 1, challengeType = Challenge.SAVING, title = "소비습관 하루에 열심히 해 화이팅 챌린지", people = 86), like = 346, likeCheck = false)
+            )
+        list.add(
+            FeedModel(
+                id = 2, profile = null, nickname = "오늘챌린지화이팅", lv = 1,
+                date = "23.05.15 14:45", title = "오늘 챌린지 인증하는데", content = "하루 만보 걷기 챌린지는 쉽고 재미있게 만보를 걸을 수있는 챌린지로, 제가 맨날 일하다가 한번 입원하고 나서 심각성을 느끼고 만들게 된 멋진 챌린지",
+                imageList = null, challengeModel = ChallengeModel(id = 1, challengeType = Challenge.SAVING, title = "소비습관 하루에 열심히 해 화이팅 챌린지", people = 86), like = 346, likeCheck = false)
+        )
+        list.add(
+            FeedModel(
+                id = 3, profile = null, nickname = "오늘챌린지화이팅", lv = 1,
+                date = "23.05.15 14:45", title = "오늘 챌린지 인증하는데", content = "하루 만보 걷기 챌린지는 쉽고 재미있게 만보를 걸을 수있는 챌린지로, 제가 맨날 일하다가 한번 입원하고 나서 심각성을 느끼고 만들게 된 멋진 챌린지",
+                imageList = null, challengeModel = ChallengeModel(id = 1, challengeType = Challenge.SAVING, title = "소비습관 하루에 열심히 해 화이팅 챌린지", people = 86), like = 346, likeCheck = false)
+        )
         _feed.value = list
     }
+
 }
