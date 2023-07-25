80183d7ef1262f440009102ede2fbe605f563afe
NaZe0320 <rwt1226@naver.com>
2023-07-26 00:42:36 +0900

    [feat] 챌린지 상세 페이지 bottom sheet 및 Tab layout

--- app/src/main/java/com/cider/cider/presentation/challenge/detail/ChallengeDetailBottomSheetFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.cider.cider.presentation.challenge.detail
-
-import com.cider.cider.R
-import com.cider.cider.databinding.FragmentChallengeDetailBottomSheetBinding
-import com.cider.cider.utils.binding.BindingBottomSheet
-
-class ChallengeDetailBottomSheetFragment: BindingBottomSheet<FragmentChallengeDetailBottomSheetBinding>(
-    R.layout.fragment_challenge_detail_bottom_sheet) {
-}
--- app/src/main/java/com/cider/cider/presentation/challenge/detail/ChallengeDetailFragment.kt
+++ app/src/main/java/com/cider/cider/presentation/challenge/detail/ChallengeDetailFragment.kt
@@ -1,18 +1,35 @@
 package com.cider.cider.presentation.challenge.detail
 
 import android.os.Bundle
+import android.util.Log
 import android.util.TypedValue
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.cider.cider.R
 import com.cider.cider.databinding.FragmentChallengeDetailBinding
+import com.cider.cider.presentation.adapter.ChallengeDetailViewPagerAdapter
 import com.cider.cider.utils.binding.BindingFragment
+import com.google.android.material.tabs.TabLayoutMediator
 
 class ChallengeDetailFragment: BindingFragment<FragmentChallengeDetailBinding>(R.layout.fragment_challenge_detail) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setBottomSheet()
+    }
 
+    private fun setBottomSheet() {
+        val pagerAdapter = ChallengeDetailViewPagerAdapter(requireActivity())
 
+        pagerAdapter.addFragment(ChallengeDetailInfoFragment())
+        pagerAdapter.addFragment(ChallengeDetailFeedFragment())
+        Log.d("TEST detail","??")
+        binding.vpChallenge.adapter = pagerAdapter
+
+        val tabTitle = listOf<String>("챌린지 정보","피드")
+        TabLayoutMediator(binding.tabLayout, binding.vpChallenge) { tab, position ->
+            tab.text = tabTitle[position]
+        }.attach()
     }
 
     private fun getPeekHeight(): Int {
@@ -20,5 +37,10 @@ class ChallengeDetailFragment: BindingFragment<FragmentChallengeDetailBinding>(R
         val peekHeightInDp = 60
         val metrics = resources.displayMetrics
         return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, peekHeightInDp.toFloat(), metrics).toInt()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        findNavController().popBackStack()
     }
 }
