08bb0d84b360809aad7de132ffd2d13eca0baf48
NaZe0320 <rwt1226@naver.com>
2023-07-26 19:36:08 +0900

    [feat] 챌린지 상세 페이지 피드 추가

--- app/src/main/java/com/cider/cider/presentation/challenge/detail/ChallengeDetailFeedFragment.kt
+++ app/src/main/java/com/cider/cider/presentation/challenge/detail/ChallengeDetailFeedFragment.kt
@@ -1,12 +1,43 @@
 package com.cider.cider.presentation.challenge.detail
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.cider.cider.R
 import com.cider.cider.databinding.FragmentChallengeDetailFeedBinding
 import com.cider.cider.databinding.FragmentChallengeDetailInfoBinding
+import com.cider.cider.presentation.adapter.FeedAdapter
+import com.cider.cider.presentation.viewmodel.ChallengeHomeViewModel
 import com.cider.cider.utils.binding.BindingFragment
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ChallengeDetailFeedFragment: BindingFragment<FragmentChallengeDetailFeedBinding>(R.layout.fragment_challenge_detail_feed) {
+
+    private val viewModel: ChallengeHomeViewModel by activityViewModels()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setFeedList()
+    }
+    private fun setFeedList() {
+        val feedAdapter = FeedAdapter(viewModel)
+
+        viewModel.testFeed(requireContext())
+
+        binding.rvFeed.apply {
+            adapter = feedAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+
+        viewModel.feed.observe(viewLifecycleOwner) {
+            viewLifecycleOwner.lifecycleScope.launch (Dispatchers.Main) {
+                feedAdapter.submitList(it)
+            }
+        }
+    }
 
     override fun onBackPressed() {
         val parentNavController = requireActivity().findNavController(R.id.fl_main)
