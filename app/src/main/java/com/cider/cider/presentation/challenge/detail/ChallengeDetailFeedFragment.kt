c5f5ce6b2617017b08ea777fa97da1bb4bb6bef0
NaZe0320 <rwt1226@naver.com>
2023-07-26 16:45:01 +0900

    [feat] 챌린지 상세 페이지 챌린지 정보 일부 구현

--- app/src/main/java/com/cider/cider/presentation/challenge/detail/ChallengeDetailFeedFragment.kt
+++ app/src/main/java/com/cider/cider/presentation/challenge/detail/ChallengeDetailFeedFragment.kt
@@ -1,9 +1,15 @@
 package com.cider.cider.presentation.challenge.detail
 
+import androidx.navigation.findNavController
 import com.cider.cider.R
 import com.cider.cider.databinding.FragmentChallengeDetailFeedBinding
 import com.cider.cider.databinding.FragmentChallengeDetailInfoBinding
 import com.cider.cider.utils.binding.BindingFragment
 
 class ChallengeDetailFeedFragment: BindingFragment<FragmentChallengeDetailFeedBinding>(R.layout.fragment_challenge_detail_feed) {
+
+    override fun onBackPressed() {
+        val parentNavController = requireActivity().findNavController(R.id.fl_main)
+        parentNavController.popBackStack()
+    }
 }
