6f8ad96822b1cbadda7beb183d3e5014e58e3f84
NaZe0320 <rwt1226@naver.com>
2023-07-28 12:21:04 +0900

    [feat] 카테고리 문구 수정

--- app/src/main/java/com/cider/cider/domain/type/challenge/Challenge.kt
+++ app/src/main/java/com/cider/cider/domain/type/challenge/Challenge.kt
@@ -7,28 +7,28 @@ enum class Challenge(val text: String, val colorResId: Int, val imageResId: Int,
         "재테크",
         R.color.btn_mint,
         R.drawable.ic_investing,
-        "돈을 버는 습관을 길러주는 재테크 챌린지를 소개해요",
+        "돈버는 습관을 길러주는 챌린지를 소개해요",
         "T"
     ),
     SAVING(
         "소비절약",
         R.color.btn_pink,
         R.drawable.ic_saving,
-        "돈을 버는 습관을 길러주는 소비절약 챌린지를 소개해요",
+        "나의 재정 상태를 파악하고, 계획적인 습관을 길러주는 챌린지를 소개해요",
         "C"
     ),
     MONEY_MANAGEMENT(
         "돈 관리",
         R.color.btn_blue,
         R.drawable.ic_money_management,
-        "돈을 버는 습관을 길러주는 돈관리 챌린지를 소개해요",
+        "재테크, 금융, 투자에 대한 지식을 쌓을 수 있는 챌린지를 소개해요",
         "M"
     ),
     FINANCIAL_LEARNING(
         "금융학습",
         R.color.btn_purple,
         R.drawable.ic_financial_learning,
-        "돈을 버는 습관을 길러주는 금융학습 챌린지를 소개해요",
+        "불필요한 지출을 줄이고, 절약하는 습관을 길러주는 챌린지를 소개해요",
         "L"
     ),
 }
