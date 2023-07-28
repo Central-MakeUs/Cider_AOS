2fb7011627d7bbcf9f922ad130d6d6cfcdf694ca
NaZe0320 <rwt1226@naver.com>
2023-07-29 02:39:26 +0900

    [feat] 챌린지 현황 도움말 구현 (balloon)

--- app/src/main/java/com/cider/cider/presentation/challenge/detail/ChallengeDetailInfoFragment.kt
+++ app/src/main/java/com/cider/cider/presentation/challenge/detail/ChallengeDetailInfoFragment.kt
@@ -2,12 +2,16 @@ package com.cider.cider.presentation.challenge.detail
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import com.cider.cider.R
 import com.cider.cider.databinding.FragmentChallengeDetailInfoBinding
 import com.cider.cider.presentation.viewmodel.ChallengeDetailViewModel
 import com.cider.cider.utils.binding.BindingFragment
+import com.skydoves.balloon.ArrowOrientation
+import com.skydoves.balloon.Balloon
+import com.skydoves.balloon.BalloonAnimation
 
 class ChallengeDetailInfoFragment: BindingFragment<FragmentChallengeDetailInfoBinding>(R.layout.fragment_challenge_detail_info) {
 
@@ -17,6 +21,28 @@ class ChallengeDetailInfoFragment: BindingFragment<FragmentChallengeDetailInfoBi
         super.onViewCreated(view, savedInstanceState)
 
         setCaution()
+        setBalloon()
+    }
+
+    private fun setBalloon() {
+        val balloon = Balloon.Builder(requireContext())
+            .setArrowSize(10)
+            .setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.gray_7))
+            .setArrowOrientation(ArrowOrientation.BOTTOM)
+            .setArrowPosition(0.5f)
+            .setWidthRatio(0.55f)
+            .setHeight(80)
+            .setTextSize(12f)
+            .setCornerRadius(4f)
+            .setAlpha(1f)
+            .setText("함께 참여하는 유저들의\n" +
+                    "이 챌린지 평균 진행 현황이에요")
+            .setBalloonAnimation(BalloonAnimation.FADE)
+            .build();
+        binding.ivCurrentGuide.setOnClickListener {
+            balloon.showAlignBottom(binding.ivCurrentGuide);
+        }
+
     }
 
     private fun setCaution() {
