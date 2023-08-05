cd3238e5b589748758640bdfd2746253d770fdfc
NaZe0320 <rwt1226@naver.com>
2023-08-05 15:58:25 +0900

    [feat] date Picker 시작일 오늘날짜

--- app/src/main/java/com/cider/cider/presentation/register/RegisterProfileFragment.kt
+++ app/src/main/java/com/cider/cider/presentation/register/RegisterProfileFragment.kt
@@ -77,11 +77,11 @@ class RegisterProfileFragment
     private fun setBirth() {
         binding.btnProfileBirth.setOnClickListener {
             val cal = Calendar.getInstance()
-            cal.set( if ((viewModel.birth.value?.year ?: cal.get(Calendar.YEAR)) <= 1900) cal.get(Calendar.YEAR) else viewModel.birth.value?.year?:0,
-                viewModel.birth.value?.month?:cal.get(Calendar.MONTH),
-                viewModel.birth.value?.day?:cal.get(Calendar.DAY_OF_MONTH))
-
-            Log.d("TEST","${cal.get(Calendar.YEAR)} / ${cal.get(Calendar.MONTH)} / ${cal.get(Calendar.DAY_OF_MONTH)}")
+            if((viewModel.birth.value?.year ?: 0) <= 1900) {
+                cal.set( if ((viewModel.birth.value?.year ?: cal.get(Calendar.YEAR)) <= 1900) cal.get(Calendar.YEAR) else viewModel.birth.value?.year?:0,
+                    viewModel.birth.value?.month?:0,
+                    viewModel.birth.value?.day?:1)
+            }
             val data = DatePickerDialog.OnDateSetListener { view, year, month, day ->
                 viewModel.changeBirth(Birth(year, month, day))
             }
