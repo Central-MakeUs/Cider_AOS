9a7a663b7696df7f10cca4185fdbb66e561bae85
NaZe0320 <rwt1226@naver.com>
2023-09-01 09:30:51 +0900

    [feat] 리스트뷰 양 여백 임시 처리

---
+++ app/src/main/java/com/cider/cider/presentation/adapter/ChallengeCard2Adapter.kt
@@ -0,0 +1,56 @@
+package com.cider.cider.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.cider.cider.databinding.ItemChallengeCard2Binding
+import com.cider.cider.databinding.ItemChallengeCardBinding
+import com.cider.cider.domain.model.ChallengeCardModel
+import com.cider.cider.presentation.viewmodel.ChallengeListViewModel
+import com.cider.cider.utils.ItemDiffCallback
+
+class ChallengeCard2Adapter(private val viewModel : ChallengeListViewModel): ListAdapter<ChallengeCardModel, RecyclerView.ViewHolder>(
+    ItemDiffCallback<ChallengeCardModel>(
+        onContentsTheSame = {old, new -> old == new},
+        onItemsTheSame = {old, new -> old.id == new.id}
+    )
+){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return ChallengeCardVieHolder(ItemChallengeCard2Binding.inflate(inflater, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = getItem(position)
+
+        when (holder) {
+            is ChallengeCardVieHolder -> {
+                holder.bind(item)
+            }
+        }
+    }
+
+    inner class ChallengeCardVieHolder(
+        private val binding: ItemChallengeCard2Binding
+    ): RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: ChallengeCardModel) {
+            binding.challenge = item
+            binding.vm = viewModel
+
+            itemView.setOnClickListener {
+                listener?.onItemClick(item.id)
+            }
+        }
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(id:Int)
+    }
+
+    private var listener: OnItemClickListener? = null
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.listener = listener
+    }
+}
